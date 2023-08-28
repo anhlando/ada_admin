489515b2fbccaf0efb2e2fb88cccc0d03d269250
Anh Lân <anhlando@yahoo.com>
2023-08-28 20:11:15 +0700

    update content models

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
@@ -56,15 +57,7 @@ module.exports = ({ env }) => {
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+          ca: fs.readFileSync(__dirname + '/ca-certificate.crt').toString(),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
