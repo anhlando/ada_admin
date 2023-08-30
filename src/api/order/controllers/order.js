66faedcf55ed8c0f3e380f9de9316050836c6645
Anh Lân <anhlando@yahoo.com>
2023-08-30 21:39:00 +0700

    update content models

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * customer controller
+ * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::customer.customer');
+module.exports = createCoreController('api::order.order');
