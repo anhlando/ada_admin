c8385d439d3277814acf895106047ff78be00da8
Anh Lân <anhlando@yahoo.com>
2023-09-06 11:26:05 +0700

    update custom controllers + models + documentation

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,37 @@
 'use strict';
 
+const { nanoid } = require('nanoid');
 /**
  * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({strapi}) => ({
+      async find(ctx) {
+        console.log('custom controller api::order.order');
+        console.log('userId: ' + ctx.state.user.id);
+        const {filters} = ctx.query;
+        ctx.query = {
+          ...ctx.query,
+          filters: {
+            ...filters,
+            userId: ctx.state.user.id
+          }
+        };
+        console.log(filters);
+        // @ts-ignore
+        return await super.find(ctx);
+      },
+      async create(ctx) {
+        ctx.request.body.data = {
+            ...ctx.request.body.data,
+            userId: ctx.state.user.id,
+            orderId: nanoid(),
+        }
+        // @ts-ignore
+        const response = await super.create(ctx);
+        return response;
+      }
+}));
+ 
---
+++ src/api/order/routes/custom.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     routes: [
+//         {
+//             // method: 'GET',
+//             // path: '/me/custom',
+//             // handler: 'order.customAction',
+//             // config: {
+
+//             // }
+//         }
+//     ]
+// }
